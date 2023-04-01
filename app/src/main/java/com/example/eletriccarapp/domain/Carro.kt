ef6e8349f0ded912ddf87d566cf74a4339dee810
Vinicius Recoaro <vrecoaro@hotmail.com>
2023-04-01 07:59:18 -0300

    Criacao inner classe para acessar API e trazer informacoes de carros

--- app/src/main/java/com/example/eletriccarapp/domain/Carro.kt
+++ app/src/main/java/com/example/eletriccarapp/domain/Carro.kt
@@ -1,7 +1,7 @@
 package com.example.eletriccarapp.domain
 
-data class Carro (
-    val id: Int,
+data class Carro(
+    val id: String,
     val price: String,
     val battery: String,
     val power: String,
--- app/src/main/java/com/example/eletriccarapp/ui/CarFragment.kt
+++ app/src/main/java/com/example/eletriccarapp/ui/CarFragment.kt
@@ -1,18 +1,23 @@
 package com.example.eletriccarapp.ui
 
-import android.content.Intent
+import android.os.AsyncTask
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.eletriccarapp.R
 import com.example.eletriccarapp.data.CarFactory
+import com.example.eletriccarapp.domain.Carro
 import com.example.eletriccarapp.ui.adapter.CarAdapter
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import org.json.JSONArray
+import org.json.JSONTokener
+import java.net.HttpURLConnection
+import java.net.URL
 
 class CarFragment: Fragment() {
 
@@ -49,7 +54,75 @@ class CarFragment: Fragment() {
 
     fun setupListeners(){
         fabCalculate.setOnClickListener{
-            startActivity(Intent(context, AutonomyCalculateActivity::class.java))
+        Mytask().execute("https://igorbag.github.io/cars-api/cars.json")
+        //startActivity(Intent(context, AutonomyCalculateActivity::class.java))
         }
+    }
+
+    inner class Mytask: AsyncTask<String, String, String>(){
+
+        override fun onPreExecute() {
+            super.onPreExecute()
+            Log.d("MyTask", "Iniciando ...")
+        }
+
+        override fun doInBackground(vararg url: String?): String {
+            TODO("Not yet implemented")
+            val urlConnection: HttpURLConnection? = null
+            try{
+                val urlBase = URL(url[0])
+                urlConnection = urlBase.openConnection() as HttpURLConnection
+                urlConnection.connectTimeout = 60000
+                urlConnection.readTimeout = 60000
+
+                var response = urlConnection.inputStream.bufferedReader().use{it.readText()}
+                publishProgress(response)
+            }catch(e: Exception){
+                Log.e("Erro", "Erro ao realizar processamento ...")
+            }finally {
+                urlConnection?.disconnect()
+                }
+            return ""
+            }
+
+        override fun onProgressUpdate(vararg values: String?) {
+            try{
+                val jsonArray = JSONTokener(values[0]).nextValue() as JSONArray
+
+                for(i in 0 until jsonArray.length()){
+                    val id = jsonArray.getJSONObject(i).getString("id")
+                    Log.d("IDDD -> ", id)
+
+                    val price = jsonArray.getJSONObject(i).getString("preco")
+                    Log.d("IDDD -> ", price)
+
+                    val battery = jsonArray.getJSONObject(i).getString("bateria")
+                    Log.d("IDDD -> ", battery)
+
+                    val power = jsonArray.getJSONObject(i).getString("potencia")
+                    Log.d("IDDD -> ", power)
+
+                    val recharge = jsonArray.getJSONObject(i).getString("recarga")
+                    Log.d("IDDD -> ", recharge)
+
+                    val urlPhoto = jsonArray.getJSONObject(i).getString("urlPhoto")
+                    Log.d("IDDD -> ", urlPhoto)
+
+                    val model = Carro(
+                        id = id,
+                        price = price,
+                        battery = battery,
+                        power = power,
+                        recharge = recharge,
+                        urlPhoto = urlPhoto
+                    )
+                    Log.d("Model -> ", model.toString())
+                }
+
+            }catch(e: Exception){
+                Log.e("Erro", e.message.toString())
+            }
+        }
+
     }
 }
